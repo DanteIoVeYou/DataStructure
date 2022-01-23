20b3c5611ed43b8b7ef6d3f877b419e9c01bd4e1
danteking <danteking@aliyun.com>
2022-01-23 16:36:42 +0800

    class Solution {

---
+++ Leetcode/2037. 使每位学生都有座位的最少移动次数.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int count = 0;
+        for(int i = 0; i < seats.size(); i++) {
+            if(seats[i] > students[i]) {
+                count += seats[i] - students[i];
+            }
+            else {
+                count += students[i] - seats[i];
+            }
+        }
+        return count;
+    }
+};
